f0fede59899a1bfb317a8a2342794ee86248a2d9
Max Gurela <maxpowa1@gmail.com>
2016-11-05 10:52:14 -0700

    Rearrange and cleanup

--- examples/modules/echo.cr
+++ examples/modules/echo.cr
@@ -1,12 +1,10 @@
 # This file is part of ircbot.cr
 # Copyright (C) 2016 Max Gurela <max.gurela@outlook.com>
 # Released under the terms of the MIT license (see LICENSE).
-require "ircbot"
+require "ircbot" # not really required because it's already in ../bot.cr but for clarity we have it here
 
-module IRCBot
-  class EchoModule < IRCBot::Handler
-    def on_privmsg(message)
-      IRCBot::Bot.instance.write("PRIVMSG #{message.params[0]} :#{message.params[1..-1]}")
-    end
+class EchoModule < IRCBot::Handler
+  def on_privmsg(message)
+    IRCBot::Bot.instance.write("PRIVMSG #{message.params[0]} :#{message.params[1..-1].join(" ")}")
   end
 end
--- src/ircbot.cr
+++ src/ircbot.cr
@@ -2,4 +2,4 @@
 # Copyright (C) 2016 Max Gurela <max.gurela@outlook.com>
 # Released under the terms of the MIT license (see LICENSE).
 
-require "./core/bot"
+require "./ircbot/bot"
